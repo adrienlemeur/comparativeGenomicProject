72552fe4cef101cc34e6ed80301c80e86b10e5e8
adrienlemeur <adrien.le-meur@u-psud.fr>
2020-11-03 14:30:15 +0100

    genome comparison 2 by 2

---
+++ genome_comparison.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+'''
+    File name: parser.py
+    Author: Adrien Le Meur
+    Date created: 03/10/2020
+    Date last modified: 03/10/2020
+    Python Version: 3.0
+'''
+
+#__________INPUT__________INPUT__________INPUT__________INPUT__________INPUT__________INPUT
+
+
+
+
+
+#__________FUNCTION__________FUNCTION__________FUNCTION__________FUNCTION__________FUNCTION__________FUNCTION
+
+#input : multi_fasta file and a genome name
+#output : a 2 level dictionnary -> genome_dict -> genome_dict[genome_name] {gene_name : DNA sequence}
+def fasta_parser(multi_fasta, genome_name)
+	genome_dict = {}
+	genome_dict[genome_name] = {}
+
+	with open(multi_fasta) as mf:
+		lines = mf.readlines()
+
+		for current_line in lines :
+			if current_line[0] == '>' :
+				gene_name = current_line
+			else :
+				genome_dict[genome_name][gene_name] = current_line.rstrip().replace('\n', '')
+	return(genome_dict)
+
+
+
+#input : 2 multifasta files names and 2 genome names
+def genome_comparison(genome_1, name_1, genome_2, name_2):
+	dict_genome_1 = fasta_parser(genome_1, name_1)
+	dict_genome_2 = fasta_parser(genome_2, name_2)
+	
+#__________MAIN__________MAIN__________MAIN__________MAIN__________MAIN__________MAIN
+
+
