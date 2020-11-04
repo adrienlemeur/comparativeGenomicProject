6b2512a8401a786ca14c06856df08a4479a743b0
adrienlemeur <adrien.le-meur@u-psud.fr>
2020-11-04 12:46:42 +0100

    TOUT FONCTIONNE SAUF LE BLASTP (DERNIERE BOUCLE)

--- main.sh
+++ main.sh
@@ -5,41 +5,66 @@
 # Récupération et organisation des données
 #------------------------------------------
 
-# On se place dans le répertoire du projet qui contient uniquement prot.tar
-tar -xvf prot.tar # dézippage
 
-# Téléchargement de l'outil blast
-wget -O ncbi-blast-2.10.1+-x64-linux.tar.gz ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.10.1+-x64-linux.tar.gz
-gunzip ncbi-blast-2.10.1+-x64-linux.tar.gz # on dézippe
-tar -xvf ncbi-blast-2.10.1+-x64-linux.tar # on dé-tar
 
-chmod +x ncbi-blast-2.10.1+/bin # droit d'utilisation sur toutes les fonctions
+#le flag --nostart permet de ne pas retélécharger les fichiers à chaque fois
+starting='TRUE'
 
-# Création des 27 bases de données
-if [ ! -f Blast_db ];then
-  mkdir Blast_db
+while [ ! $# -eq 0 ]
+do
+	case "$1" in
+		--nostart)
+			starting='FALSE'
+			;;
+	esac
+	shift
+done
+
+if [ $starting = 'TRUE' ]
+then
+	echo $starting
+
+	# On se place dans le répertoire du projet qui contient uniquement prot.tar
+	tar -xvf prot.tar # dézippage
+
+	# Téléchargement de l'outil blast
+	wget -O ncbi-blast-2.10.1+-x64-linux.tar.gz ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.10.1+-x64-linux.tar.gz
+	gunzip ncbi-blast-2.10.1+-x64-linux.tar.gz # on dézippe
+	tar -xvf ncbi-blast-2.10.1+-x64-linux.tar # on dé-tar
+	rm ncbi-blast-2.10.1+-x64-linux.tar
+
+	chmod +x ncbi-blast-2.10.1+/bin # droit d'utilisation sur toutes les fonctions
+
+
+	# Création des 27 bases de données
+	if [ ! -f Blast_db ];then
+	  mkdir -p Blast_db
+	fi
+
+	for A in prot/*.fa
+	do
+		./ncbi-blast-2.10.1+/bin/makeblastdb -in $A -dbtype "prot" -out Blast_db/$(basename $A ".fa")
+	done
 fi
 
-for A in prot/* do
-  basenm = basename $A
-  /ncbi-blast-2.10.1+/bin/makeblastdb -in prot/A -dbtype "prot" -out Blast_db/basenm
-done 
 
 # Dossier de sortie des alignements
-if [ ! -f Blast_ouput ];then
-  mkdir Blast_ouput
+if [ ! -f Blast_output ];then
+	mkdir -p Blast_output
 fi
 
 # Réalisation des 21*21 alignements
-for A in prot/* do
-  for B in prot/* do
-    database = basename $B
-    base_name = basename $A
-    output_name = paste base_name _ database # A_B
-    echo $output_name
-    ./ncbi-blast-2.10.1+/bin/blastp -query prot/base_name.fa -db Blast_db/database -out Blast_output/output_name.txt -outfmt '7 qseqid sseqid pident length mismatch gapopen qstart qen sstart send evalue bitscore qlen slen gaps'
-  done 
-done 
+for A in prot/*.fa;do
+	for B in Blast_db/*.pdb;do
+
+	output_name= echo $(basename $A ".fa")_$(basename $B ".fa")
+
+	echo $A
+	echo $B
+	
+	./ncbi-blast-2.10.1+/bin/blastp -query $A -db $B -out Blast_output/$(basename $A ".fa")_$(basename $B ".pdb") -outfmt '7 qseqid sseqid pident length mismatch gapopen qstart qen sstart send evalue bitscore qlen slen gaps'
+	done
+done
 
 # Fin de cette partie : 21x21 fichiers txt
 
