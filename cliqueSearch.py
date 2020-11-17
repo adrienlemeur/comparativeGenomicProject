b2d664d13ea2be6154c1f88f0a427a960c400352
LeilaOutem <63793872+LeilaOutem@users.noreply.github.com>
2020-11-17 09:58:13 +0100

    Update cliqueSearch.py

--- cliqueSearch.py
+++ cliqueSearch.py
@@ -19,12 +19,12 @@ G = nx.read_edgelist(filename,delimiter='	') # les genes stockes dans le fichier
 
 for clq in nx.clique.find_cliques(G): #la fonction find_cliques permet de renvoyer toutes les cliques maximales dans un graphe non oriente G
 	print(len(clq))
-	if len(clq) < 21: #si la clique ne fait pas intervenir tous les génomes, on les stocke dans un fichier à part (cliques_pas_max)
+	if len(clq) < 21: #si la clique ne fait pas intervenir tous les genomes, on les stocke dans un fichier a part (cliques_pas_max)
 		for clq_item in clq:
 			f.write(clq_item.decode("utf-8"))
 			f.write("\t")
 		f.write("\n")
-	else :#cliques faisant intervenir tous les génomes
+	else :#cliques faisant intervenir tous les genomes
 		for clq_item in clq:
 			f2.write(clq_item.decode("utf-8"))
 			f2.write("\t")
@@ -32,3 +32,4 @@ for clq in nx.clique.find_cliques(G): #la fonction find_cliques permet de renvoy
 		
 f.close()
 f2.close()
+
