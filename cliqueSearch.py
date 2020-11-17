096dc2cd8b449c64a8cf114eb36ec296b056c414
LeilaOutem <outemleila934@gmail.com>
2020-11-17 09:31:36 +0100

    ajout du parmètre de taille

--- cliqueSearch.py
+++ cliqueSearch.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import networkx as nx #import de la librairie networkx pour l'utilisation de la fonction find_cliques()
 import sys
 
@@ -10,15 +8,27 @@ try :
 except :
 	sys.exit()
 
-f = open("output.txt", 'w')#fichier de sortie pour stocker les resultats
+f = open("cliques_pas_max.txt", 'w')#fichier de sortie pour stocker les resultats
+
+f2 = open("cliques_max.txt", 'w')
 
 G = nx.read_edgelist(filename,delimiter='	') # les genes stockes dans le fichier d'entree sont des couples de genes homologues
 #Chaque couple correspond a une arete d'un graphe
 #La fonction read_edgelist va donc permettre de lire la liste des aretes pour permettre a la fonction qui suit de trouver les cliques maximales
 
-for clq in nx.clique.find_cliques(G): #la fonction find_cliques permet de renvoyer toutes les cliques maximales dans un graphe non oriente G
-	f.write(str(clq)) #noter la clique trouvee dans le fichier de sortie, en prenant garde de caster le resultat en string
-	f.write("\n")
-	
-f.close()
 
+for clq in nx.clique.find_cliques(G): #la fonction find_cliques permet de renvoyer toutes les cliques maximales dans un graphe non oriente G
+	print(len(clq))
+	if len(clq) < 21: #si la clique ne fait pas intervenir tous les génomes, on les stocke dans un fichier à part (cliques_pas_max)
+		for clq_item in clq:
+			f.write(clq_item.decode("utf-8"))
+			f.write("\t")
+		f.write("\n")
+	else :#cliques faisant intervenir tous les génomes
+		for clq_item in clq:
+			f2.write(clq_item.decode("utf-8"))
+			f2.write("\t")
+		f2.write("\n")
+		
+f.close()
+f2.close()
