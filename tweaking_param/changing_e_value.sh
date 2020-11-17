76d57cca9d5c0f67296d6ea22927cdbf2cd89de5
adrienlemeur <71882657+adrienlemeur@users.noreply.github.com>
2020-11-17 18:07:38 +0100

    Sans conviction

--- tweaking_param/changing_e_value.sh
+++ tweaking_param/changing_e_value.sh
@@ -3,21 +3,26 @@ clear
 
 files="ishouldnotsearchthis"
 
+mkdir -p test_folder
+i=1
+
 while read A;do
+	echo $i
 	files="$files|$A"
-	OUTPUT=$(ls artificial_files|grep -E $files)
+		
+	OUTPUT=$(find blast_outputs|grep -E $files -w)
+	echo $OUTPUT
 
-	#cat
-	#super_finder
-	#clique_search
+	#echo "#Genomes:\n"$files | sed -e 's/ishouldnotsearchthis'\|'/#/g' | sed -e 's/'\|'/\\n#/g' > test_folder/$i"_genomes.txt"
+	#cat $OUTPUT | grep "^[^#;]" | cut -f 1,2,3,4,12 >> test_folder/$i"_genomes.txt"
 
+	i=$((i + 1))
 done < strain_names.txt
 
 
 
 #Avec les seuils
-
-
+<<///
 while read A;do
 
 	echo
@@ -26,3 +31,4 @@ while read A;do
 	#clique_search
 
 done < evalue_threshold.txt
+///
