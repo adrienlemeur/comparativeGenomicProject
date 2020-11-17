96c829ce29bf61536ca74612793271daeaf23cc7
audrey-onfroy <71872404+audrey-onfroy@users.noreply.github.com>
2020-11-17 10:45:03 +0100

    Update supairFinder.py

--- supairFinder.py
+++ supairFinder.py
@@ -23,7 +23,9 @@ try:
 except :
 	outputname = "putative_orthologues.txt"
 
+print(seuil_id, seuil_cov, seuil_eval)
 seuils = [seuil_id, seuil_cov, -seuil_eval] # on met un - pour que toutes les comparaisons soient des >
+
 
 #__________MAIN__________MAIN__________MAIN__________MAIN__________MAIN__________MAIN
 
