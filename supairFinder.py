60187ccf36e2dd76ed98a8f0671efe86d0e4da46
audrey-onfroy <71872404+audrey-onfroy@users.noreply.github.com>
2020-11-23 20:44:20 +0100

    Update supairFinder.py

--- supairFinder.py
+++ supairFinder.py
@@ -80,16 +80,13 @@ with open(outputname, 'a') as po:
 			
 			if genomeB_gene_genomeA in dict:
 				# Si la valeur du best-hit de notre best hit est égal à celle de notre query, les deux gènes sont réciproques
+				print(genomeB_gene_genomeA) ; print(genomeA_gene_genomeB)
 				if dict[genomeB_gene_genomeA][0] == dict[genomeA_gene_genomeB][1] :
 					# vérification des seuils du couple
 					membre1 = dict[genomeA_gene_genomeB][2:4+1] # liste avec le % d'identite, la couverture et - la e-value
 					membre2 = dict[genomeB_gene_genomeA][2:4+1]
 					# on teste tous les seuils : membrex > seuils est une liste de trois booléens
 					# pour que les trois seuils soient validés, il faut que la comparaison donne TRUE
-					print('suivant')
-					print(membre1)
-					print(membre2)
-					print(seuils)
 					if (membre1 > seuils) and (membre2 > seuils) :
 						#on écrit le couple de gènes query / best-hit à la suite de notre doc résultat
 						po.write(i[0]+"\t"+i[1]+"\n")
