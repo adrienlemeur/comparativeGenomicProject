434e7f59381a6a3b778b6c9e71519068c060d118
audrey-onfroy <71872404+audrey-onfroy@users.noreply.github.com>
2020-11-23 19:19:30 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -114,4 +114,5 @@ mkdir -p cliques # Répertoire de sortie de cliqueSearch
 python3 cliqueSearch.py -i "reciprocity/reciprocity_list.txt" -o "cliques/cliques_max.txt" "cliques/cliques_pas_max.txt"
 
 test -s cliques/cliques_pas_max.txt || echo "Il y a eu un problème lors de la détermination des cliques. Le fichier cliques_pas_max.txt est vide ou n'existe pas."
-echo "Le nombre de cliques maximales et donc d'éléments dans le core génome est de : " wc -l cliques/cliques_max.txt
+echo "Le nombre de cliques maximales et donc d'éléments dans le core génome est de : "
+wc -l cliques/cliques_max.txt
