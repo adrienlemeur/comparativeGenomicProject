4a47e98e689aa9de56565a8bf4893f73ea6bd390
audrey-onfroy <71872404+audrey-onfroy@users.noreply.github.com>
2020-11-24 13:43:17 +0100

    Update micro_main.sh

--- micro_main.sh
+++ micro_main.sh
@@ -36,3 +36,6 @@ done
 cat blast_outputs/*.bl | grep "^[^#;]" | cut -f 1,2,3,4,12 > "reciprocity/best_hits_list.txt"
 python3 supairFinder.py -i "reciprocity/best_hits_list.txt" -o "reciprocity/reciprocity_list.txt" --seuil_identite ${identity} --seuil_coverage ${coverage} --seuil_evalue ${evalue}
 python3 cliqueSearch.py -i "reciprocity/reciprocity_list.txt" 21 -o "cliques/cliques.txt" "cliques/cliques_max.txt"
+
+core_genome_size=$(cat cliques/cliques_max.txt | wc -l)
+echo "Le nombre de cliques maximales et donc d'éléments dans le core génome est de : "${core_genome_size}"."
