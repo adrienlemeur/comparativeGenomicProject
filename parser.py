d67db0a877566e33c92d4812b1f1869d47c6ff02
LeilaOutem <63793872+LeilaOutem@users.noreply.github.com>
2020-12-01 12:48:26 +0100

    Update parser.py

--- parser.py
+++ parser.py
@@ -8,7 +8,7 @@ import sys, os
 try:
 	alignment = sys.argv[sys.argv.index("-i")+1]
 	output_igorf = sys.argv[sys.argv.index("-o")+1]
-  output_cds = sys.argv[sys.argv.index("-o")+2]
+  	output_cds = sys.argv[sys.argv.index("-o")+2]
   
 except :
 	sys.exit()
@@ -23,7 +23,6 @@ except :
 with open(alignment) as ag:
 	lines = ag.readlines()
 
-dict = {}
 
 # Fichier de sortie
 os.system("rm -f "+output_igorf)
@@ -35,14 +34,14 @@ with open(output_igorf, 'a') as igorf :
 		  for line in lines :
 			  if regex.match(line) : #pour trouver la derniere ligne avec un commentaire #
 				  line_interest = lines[i+1] #on recupere la ligne qui nous interesse, celle qui suit la derniere ligne en commentaire
-          values = line_interest.split()#on recupere les valeurs des differents champs
-          query_id = values[0].split("_")
-          subject_id = values[1].split("_")
-          evalue = values[10]
-          if (len(query_id)== 4 && len(subject_id) == 4) :
-            igorf.write(values[0]+"\t"+values[1]+"\t"+evalue+"\n")
-          elif (len(query_id)== 3 && len(subject_id) == 3) :
-            cds.write(values[0]+"\t"+values[1]+"\t"+evalue+"\n")
+				  values = line_interest.split()#on recupere les valeurs des differents champs
+				  query_id = values[0].split("_")
+				  subject_id = values[1].split("_")
+				  evalue = values[10]
+				  if (len(query_id)== 4 && len(subject_id) == 4) :
+				    igorf.write(values[0]+"\t"+values[1]+"\t"+evalue+"\n")
+				  elif (len(query_id)== 3 && len(subject_id) == 3) :
+				    cds.write(values[0]+"\t"+values[1]+"\t"+evalue+"\n")
           
             
     
