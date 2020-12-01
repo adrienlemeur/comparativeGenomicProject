49bf5e9b2ef40c8a632a92c10efbd0811d03a2b0
LeilaOutem <63793872+LeilaOutem@users.noreply.github.com>
2020-12-01 11:47:30 +0100

    Update evalue_distrib.sh

--- evalue_distrib.sh
+++ evalue_distrib.sh
@@ -15,7 +15,6 @@ if [ $starting = 'TRUE' ];then
   #Creating a directory for the downloaded outputs
   mkdir -p Blast_outputs
   wget -O Blast_yeasts.tar.gz transfert.u-psud.fr/nq01n
-  gzip -dc Blast_yeasts.tar.gz > Blast_yeasts.tar
   tar -xzvf Blast_yeasts.tar -O Blast_outputs/
 fi
 
