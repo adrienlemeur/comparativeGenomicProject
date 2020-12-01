474fc54d2434a45ee39f4e8be6d325e39c7ca11d
adrienlemeur <adrien.le-meur@u-psud.fr>
2020-12-01 14:39:54 +0100

    paralleliser les tâches avec GNU Parallel (nécessite installation)

--- paramain.sh
+++ paramain.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
 
-#parallel sh paramain.sh ::: 70 ::: 60 ::: seq 2 21 :::: ./evalue_thresholds.txt
+#parallel --joblog time.log --jobs 2 sh paramain.sh ::: 70 ::: 60 :::: evalue_thresholds.txt
 
 starting="FALSE"
 coverage=$1
 identity=$2
 eval_from_file=$3
 
-# supairFinder ne conserve que les bests hits et filtre certaines query dont certain attributs sont inférieurs à un certain seuils
-# Entrée : sortie du précédent
-# Sortie : liste des best hits réciproques
 
 python3 supairFinder.py -i "reciprocity/best_hits_list.txt" -o "reciprocity/reciprocity_list_$eval_from_file.txt" --seuil_identite ${identity} --seuil_coverage ${coverage} --seuil_evalue ${eval_from_file}
 
-mkdir -p cliques # Répertoire avec tous les résultats s'il n'existe pas déjà
+mkdir -p cliques
 
 for nombre_genome in $(seq 2 21)
 do
