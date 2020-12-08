067b516eedcf1cc09c207cb005fbafc7ce2edaef
adrienlemeur <adrien.le-meur@u-psud.fr>
2020-12-08 09:42:10 +0100

    yes

--- analyse.R
+++ analyse.R
@@ -5,6 +5,7 @@ cds_igorf <- scan('output_eval/output_eval_CDS_IGORF.txt', what=numeric(), sep="
 igorf_cds <- scan('output_eval/output_eval_IGORF_CDS.txt', what=numeric(), sep=" ", quiet=TRUE)
 igorf_igorf <- scan('output_eval/output_IGORF_IGORF.txt', what=numeric(), sep=" ", quiet=TRUE)
 
+svg("");
 par(mfrow = c(2,2), mai = rep(0.15, 4))
 hist(-log10(cds_cds), freq = F, breaks = 150, col = "royalblue1", xlim = c(0, 25), main = "CDS / CDS")
 hist(-log10(cds_igorf), freq = F, breaks = 150, col = "firebrick", xlim = c(0, 25), main = "CDS / IGORF")
